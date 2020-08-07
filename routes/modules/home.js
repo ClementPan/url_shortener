9cf8dc1dd6bdecab2ec900f3fd9161dcc5059df0
ClementPan <zzz2999582@gmail.com>
2020-08-08 00:35:49 +0800

    feat: add POST to router, output.hbs can return url created by users stored in MongoDB

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const Url = require('../../models/url')
 
 router.get('/', (req, res) => {
   res.render('index')
 })
 
 router.post('/', (req, res) => {
-  const url = req.body.url
-  res.render('output', { url })
+  const reqUrl = req.body.url
+  return Url.create({ url: reqUrl })
+    .then(() => {
+      Url.findOne({ url: reqUrl })
+        .lean()
+        .then(url => res.render('output', { url: url._id }))
+        .catch(err => console.error(err))
+    })
+    .catch(err => console.error(err))
 })
 
 module.exports = router
